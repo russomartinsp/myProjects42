746efa5e3a95b7c7288490f60c1b5a2970d115f0
Russo Dos Santos Martins <rusdos-s@c1r8p1.42sp.org.br>
2025-09-18 20:59:03 -0300

    Some alterations on GNL

--- 02_CommonCore/02_get_next_line/get_next_line.c
+++ 02_CommonCore/02_get_next_line/get_next_line.c
@@ -5,27 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rusdos-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/18 18:00:47 by rusdos-s          #+#    #+#             */
-/*   Updated: 2025/09/18 18:36:54 by rusdos-s         ###   ########.fr       */
+/*   Created: 2025/09/18 20:41:48 by rusdos-s          #+#    #+#             */
+/*   Updated: 2025/09/18 20:54:16 by rusdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-	A Função:
-    {
-	 Ele lê uma linha de um arquivo. Uma linha por vez;
-	 Uma linha termina com '\n'.
-	}
-	Processo:
-	{
-		1. É indicado um arquivo para a função - vai ser aberto com open();
-		2. É necessário um utilitário que conte os caracteres até um \n ou o final do arquivo;
-		3. Com read(), o programa vai ler um número de bytes que vai dar o utilitário.;
-		4. Como a função deve devolver uma string, o que foi lido pelo read() deve ser depositado numa 
-		variável.
-	}
-	Meus Problemas:
-	{
-		* Ainda não sei porquê precisar ser uma var estática.
-	}
-*/
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
