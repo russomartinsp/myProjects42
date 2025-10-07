c5312871345f0373363524458cdb11a80af1fc53
Russo Dos Santos Martins <rusdos-s@c2r11p6.42sp.org.br>
2025-10-07 20:37:23 -0300

    updates on ft_printf

--- 02_CommonCore/01_printf/ft_printf.c
+++ 02_CommonCore/01_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rusdos-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 14:56:30 by rusdos-s          #+#    #+#             */
-/*   Updated: 2025/09/30 21:28:45 by rusdos-s         ###   ########.fr       */
+/*   Updated: 2025/10/07 19:31:55 by rusdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,32 @@
 
 int	ft_print(const char *str, ...)
 {
+	va_list	args;
+	char	*print;
+	int	cont;
+	int	i;
+
+	va_start(args);
+	cont = 0;
+	while (str)
+	{
+		if (str[i] = "%")
+		{
+			if (checking (str[i + 1]) = )
+			
+		}
+		cont++;
+	}
+
+	return (cont);
 /*
-	1. Recebe um número variável de variáveis;
-	2. A primeira var deve ser uma string;
-	3. O programa tem quer cada char da string, contanto o número de caracteres;
-	4. Ao encontrar um "%", ele checa o que há depois e isso vai definir o tipo de variável para o va_arg;
 	5. Ele vai pegar o que tiver no próximo arg e juntar com o tipo indentificado em 4, para escrever o que está ali e contar quantos carecteres tem ali;
 	6. Ele vai continuar repetindo isso até chegar em um \0;
 	7. O programa devolve o int com o número de caracteres escritos.
 */
+}
+
+int	checking(const char *var, int cont)
+{
+	
 }
