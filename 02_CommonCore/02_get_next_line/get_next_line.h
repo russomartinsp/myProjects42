31547ba0352376bb2cad5ea4936c4a29aa307342
Russo Dos Santos Martins <rusdos-s@c1r1p1.42sp.org.br>
2025-10-16 19:10:48 -0300

    gettubf gnl done pt.1

--- 02_CommonCore/02_get_next_line/get_next_line.h
+++ 02_CommonCore/02_get_next_line/get_next_line.h
@@ -6,8 +6,27 @@
 /*   By: rusdos-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 20:44:41 by rusdos-s          #+#    #+#             */
-/*   Updated: 2025/09/18 20:57:40 by rusdos-s         ###   ########.fr       */
+/*   Updated: 2025/10/16 19:04:06 by rusdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NET_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef OPEN_MAX
+#  define BUFFER_SIZE 10240
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
--- 02_CommonCore/02_get_next_line/get_next_line_utils.c
+++ 02_CommonCore/02_get_next_line/get_next_line_utils.c
@@ -6,8 +6,71 @@
 /*   By: rusdos-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 17:21:31 by rusdos-s          #+#    #+#             */
-/*   Updated: 2025/09/19 20:32:30 by rusdos-s         ###   ########.fr       */
+/*   Updated: 2025/10/16 19:04:41 by rusdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char )c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+static char	*ft_strdup(char *str, char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+
+	i = -1;
+	while (s1[++i])
+		str[i] == s1[i];
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1);
+		if (!s1)
+			return (NULL);
+		*s1 = '\0';
+	}
+	if (!s2)
+		return (s1);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	str = ft_strdup(str, s1, s2);
+	free(s1);
+	return (str);
+}
